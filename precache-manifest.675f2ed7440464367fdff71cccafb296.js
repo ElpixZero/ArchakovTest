5682aa05d3a1309254ae2facf4b2941f1bffc4ad
ElpixZero <elnasurov@gmail.com>
2020-01-30 20:40:46 +0300

    Updates

---
+++ precache-manifest.675f2ed7440464367fdff71cccafb296.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44bd4026044fe049c89fe06beb72392",
+    "url": "/ArchakovTest/index.html"
+  },
+  {
+    "revision": "f747bbb30afc43b40977",
+    "url": "/ArchakovTest/static/js/2.dc2df6b3.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/ArchakovTest/static/js/2.dc2df6b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9036a2e9c10dc93fbdd3",
+    "url": "/ArchakovTest/static/js/main.446b6663.chunk.js"
+  },
+  {
+    "revision": "30d3eae8d7bb43c0bbbd",
+    "url": "/ArchakovTest/static/js/runtime-main.0f7c8377.js"
+  }
+]);
